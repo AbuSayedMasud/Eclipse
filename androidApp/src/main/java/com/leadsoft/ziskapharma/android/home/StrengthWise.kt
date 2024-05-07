2cfd89ead1e591f2741df752658974fb0d9e9086
masud <sayed.masud@lead-bd.com>
2024-05-07 10:44:05 +0600

    strength ,category wise data added

--- androidApp/src/main/java/com/leadsoft/ziskapharma/android/home/StrengthWise.kt
+++ androidApp/src/main/java/com/leadsoft/ziskapharma/android/home/StrengthWise.kt
@@ -1,9 +1,174 @@
 package com.leadsoft.ziskapharma.android.home
 
+import android.content.Context
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.leadsoft.ziskapharma.android.R
+import com.leadsoft.ziskapharma.android.api.brandwise.BrandWiseData
+import com.leadsoft.ziskapharma.android.api.brandwise.Table
+import com.leadsoft.ziskapharma.android.home.common.ContentCell
+import com.leadsoft.ziskapharma.android.home.common.HeaderCell
+import com.leadsoft.ziskapharma.android.repository.RouteSearchHttpRepository
+import com.leadsoft.ziskapharma.android.theme.PrimaryColor
+import com.leadsoft.ziskapharma.android.theme.ZiskaTheme
+import com.leadsoft.ziskapharma.android.theme.rememberWindowSizeClass
 
 @Composable
 fun StrengthWise() {
-    Text(text = "hello4")
+    val context: Context = LocalContext.current
+    val result: List<BrandWiseData> by remember {
+        mutableStateOf(RouteSearchHttpRepository(context).searchRoutes())
+    }
+    val brandWiseResult: List<BrandWiseData> by remember {
+        mutableStateOf(RouteSearchHttpRepository(context).searchBrandSummery())
+    }
+    val window = rememberWindowSizeClass()
+    val headers = listOf(
+        "Brand",
+        "Budget",
+        "TP Sale",
+        "Ach %",
+        "Net Ach %",
+    )
+    ZiskaTheme(window) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Sales Report",
+                    color = Color.Black,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(
+                            top = 8.dp,
+                            bottom = 8.dp,
+                            end = 16.dp,
+                            start = 4.dp
+                        ),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Card(
+                    elevation = 4.dp,
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier.size(40.dp)
+                ) {
+                    IconButton(
+                        onClick = { /* Handle filter button click */ },
+                        modifier = Modifier
+                            .size(30.dp)
+                            .padding(8.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.filter),
+                            contentDescription = "Filter",
+                            tint = PrimaryColor
+                        )
+                    }
+                }
+            }
+            Row(verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 24.dp)) {
+                Surface(
+                    shadowElevation = 8.dp,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp)),
+                    tonalElevation = 4.dp
+
+                ) {
+                    Table(
+                        modifier = Modifier,
+                        columnCount = headers.size,
+                        rowCount = brandWiseResult.size + 1,
+                        stickyRowCount = 1,
+                        stickyColumnCount = 1,
+                        maxCellWidthDp = 320.dp
+                    ) { rowIndex, columnIndex ->
+                        val header = headers[columnIndex]
+                        if (rowIndex == 0) {
+                            HeaderCell(header)
+                        } else {
+                            val r = brandWiseResult[rowIndex - 1]
+                            when (header) {
+                                "Brand" -> ContentCell(rowIndex, r.brand)
+                                "Budget" -> ContentCell(rowIndex, r.budget)
+                                "TP Sale" -> ContentCell(rowIndex, r.tpSale)
+                                "Ach %" -> ContentCell(rowIndex, r.ach)
+                                "Net Ach %" -> ContentCell(rowIndex, r.netAch)
+                            }
+                        }
+                    }
+                }
+            }
+            Row(verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 24.dp, bottom = 60.dp)) {
+                Surface(
+                    shadowElevation = 8.dp,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp)),
+                    tonalElevation = 4.dp
+
+                ) {
+                    Table(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        columnCount = headers.size,
+                        rowCount = result.size + 1,
+                        stickyRowCount = 1,
+                        stickyColumnCount = 1,
+                        maxCellWidthDp = 320.dp
+                    ) { rowIndex, columnIndex ->
+                        val header = headers[columnIndex]
+                        if (rowIndex == 0) {
+                            HeaderCell(header)
+                        } else {
+                            val r = result[rowIndex - 1]
+                            when (header) {
+
+                                "Brand" -> ContentCell(rowIndex, r.brand)
+                                "Budget" -> ContentCell(rowIndex, r.budget)
+                                "TP Sale" -> ContentCell(rowIndex, r.tpSale)
+                                "Ach %" -> ContentCell(rowIndex, r.ach)
+                                "Net Ach %" -> ContentCell(rowIndex, r.netAch)
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
