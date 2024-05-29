c8c533f4aeaf3ad0e2731d09286a68541808ff1b
sayed.masud <sayed.masud@leads-bd.com>
2024-05-29 13:29:07 +0600

    comments and button section add

--- androidApp/src/main/java/com/leadsoft/ziskapharma/android/request/creditCardRequest/CreditCustomerRequestView.kt
+++ androidApp/src/main/java/com/leadsoft/ziskapharma/android/request/creditCardRequest/CreditCustomerRequestView.kt
@@ -1,5 +1,9 @@
 package com.leadsoft.ziskapharma.android.request.creditCardRequest
 
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -16,6 +20,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
@@ -25,16 +31,26 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.toColorInt
+import com.leadsoft.ziskapharma.android.R
+import com.leadsoft.ziskapharma.android.activity.HomeActivity
 import com.leadsoft.ziskapharma.android.api.PieChartData
+import com.leadsoft.ziskapharma.android.biometric.BioMetricPrompt
 import com.leadsoft.ziskapharma.android.request.pieChart.PieChartCompose
+import com.leadsoft.ziskapharma.android.theme.AppTheme
+import com.leadsoft.ziskapharma.android.theme.PrimaryColor
+import com.leadsoft.ziskapharma.android.theme.White
+import com.leadsoft.ziskapharma.android.theme.getCardColors
+import com.leadsoft.ziskapharma.android.theme.secondarayColor
 
 @Composable
 fun CreditCustomerRequestView() {
+    val (backgroundColor, contentColor) = getCardColors()
     val paddingValue = if (isSystemInDarkTheme()) {
         6.dp
     } else {
@@ -72,7 +88,7 @@ fun CreditCustomerRequestView() {
                 Text(
                     text = "Overview",
                     fontSize = 25.sp,
-                    color = Color.Gray,
+                    color = contentColor,
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier.padding(bottom = 16.dp) // Add bottom padding to separate the two cards
                 )
@@ -129,11 +145,65 @@ fun CreditCustomerRequestView() {
         }
         CustomCard()
         EffectiveCard()
+        CommentsView()
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp,end=16.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            // First Button with 2f width
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .weight(2f)
+                    .padding(top = AppTheme.dimens.large),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = PrimaryColor,
+                    contentColor = White,
+                ),
+                shape = RoundedCornerShape(10.dp),
+            ) {
+                Text(
+                    text = "Submit",
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(3.dp),
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            // Second Button with 0.5f width
+            Button(
+                onClick = {
+                },
+                modifier = Modifier
+                    .weight(0.5f)
+                    .padding(top = AppTheme.dimens.large),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = secondarayColor, // Example color
+                    contentColor = Color.White, // Example color
+                ),
+                shape = RoundedCornerShape(10.dp),
+            ) {
+                Text(
+                    text = "New",
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(3.dp),
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
     }
 }
 
 @Composable
 fun RequestItem(label: String, value: String, indicatorColor: Color) {
+    val (backgroundColor, contentColor) = getCardColors()
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
@@ -150,14 +220,14 @@ fun RequestItem(label: String, value: String, indicatorColor: Color) {
         Text(
             text = label,
             fontSize = 16.sp,
-            color = Color.Gray,
-            fontWeight = FontWeight.Bold
+            color = contentColor,
+            fontWeight = FontWeight.Normal
         )
         Spacer(modifier = Modifier.weight(1f))
         Text(
             text = value,
             fontSize = 16.sp,
-            color = Color.Gray,
+            color = contentColor,
             fontWeight = FontWeight.Bold
         )
     }
--- androidApp/src/main/java/com/leadsoft/ziskapharma/android/request/creditCardRequest/CustomerCardView.kt
+++ androidApp/src/main/java/com/leadsoft/ziskapharma/android/request/creditCardRequest/CustomerCardView.kt
@@ -37,7 +37,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.toColorInt
 import com.leadsoft.ziskapharma.android.theme.White
 import com.leadsoft.ziskapharma.android.theme.getCardColors
 import com.leadsoft.ziskapharma.android.theme.secondarayColor
